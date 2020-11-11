490e0169df6473bdb7c780bba013420c1e54686c
KevinAp-5 <keven.clash3@gmail.com>
2020-11-11 00:22:47 -0300

    removed useless code

--- wiki.py
+++ wiki.py
@@ -10,22 +10,19 @@ def main(string_input, **keyword_password):
         try:
             x = wikipedia.search(string_input)[:limit]
         except IndexError:
-            print('Não encontrado')
+            print('Not found.')
     else:
         x = wikipedia.search(string_input)[0]
  
     for a in x:
-        print(a)
         i = wikipedia.page(a)
         url = i.url
-        try:
-            image = i.images
-        except IndexError:
-            print('Fotos não disponíveis.')
         print(f'{""*25}\n{a}:\n{wikipedia.summary(a, sentences=1)}\n{url}')
 
 
 if __name__ == '__main__':
     x = input('search: ')
-    a = main(x, lang='pt', limit=1)
+    y = input('lang: ')
+    z = int(input('limit: '))
+    a = main(x, lang=y, limit=z)
 
